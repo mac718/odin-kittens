cf238a78c521c2056e38c2493579e12590402a7e
mac718 <mac718@gmail.com>
2018-04-08 09:07:46 -0400

    Set up Kitten model and RESTful routes, controller actions, and views

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project: Kittens API
+
+Practice setting up a Rails app to be a data-producing API. From the [Odin Project](https://www.theodinproject.com/courses/ruby-on-rails/lessons/apis?ref=lnav) curriculum.
---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,53 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save 
+      flash[:success] = "Kitten saved!"
+      redirect_to kittens_path
+    else
+      flash.now[:danger] = "Could not save this kitten to the database."
+      render :new
+    end
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.save
+      flash[:succes] = "Kittin info updated!"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Could not update kitten info."
+      render :edit
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    flash[:success] = "Kitten deleted from datebase."
+    redirect_to '/kittens'
+  end
+
+  private 
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
+end
